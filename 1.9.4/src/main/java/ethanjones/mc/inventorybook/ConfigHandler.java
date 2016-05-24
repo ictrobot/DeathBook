6fe7f04899234e93054fad39884a97b10da76511
ictrobot <ictrobot@outlook.com>
2016-05-24 20:04:17 +0100

    Add 1.9.4 version

---
+++ 1.9.4/src/main/java/ethanjones/mc/inventorybook/ConfigHandler.java
@@ -0,0 +1,39 @@
+package ethanjones.mc.inventorybook;
+
+import net.minecraftforge.common.config.Configuration;
+
+import java.io.File;
+
+public class ConfigHandler {
+  // BOOK
+  public static int LINES_PER_PAGE;
+  public static int CHAR_PER_LINE;
+  // NBT
+  public static int NBT_PER_ITEM;
+  public static boolean NBT_EXTRA_PAGE;
+  // DEATH
+  public static boolean DEATH_BOOK;
+  public static boolean DEATH_REQUIRE_BOOK_AND_QUILL;
+  // OTHER
+  public static boolean RIGHT_CLICK_PLAYER_INVENTORY;
+  public static boolean RIGHT_CLICK_BLOCK_INVENTORY;
+
+  public static void init(File file) {
+    Configuration config = new Configuration(file);
+    config.load();
+
+    LINES_PER_PAGE = config.get("BOOK", "LINES_PER_PAGE", 14).getInt();
+    CHAR_PER_LINE = config.get("BOOK", "CHAR_PER_LINE", 18).getInt();
+
+    NBT_PER_ITEM = config.get("NBT", "ALLOWED_LENGTH_PER_ITEM", 32767 / 20).getInt();
+    NBT_EXTRA_PAGE = config.get("NBT", "EXTRA_PAGE_FOR_ITEM", true, "Will create a page just for an item if the NBT is too long").getBoolean();
+
+    DEATH_BOOK = config.get("DEATH", "DEATH_BOOK", true).getBoolean();
+    DEATH_REQUIRE_BOOK_AND_QUILL = config.get("DEATH", "DEATH_REQUIRE_BOOK_AND_QUILL", false).getBoolean();
+
+    RIGHT_CLICK_PLAYER_INVENTORY = config.get("OTHER", "RIGHT_CLICK_PLAYER_INVENTORY", true, "Enable shift right clicking book and quill to receive inventory book").getBoolean();
+    RIGHT_CLICK_BLOCK_INVENTORY = config.get("OTHER", "RIGHT_CLICK_BLOCK_INVENTORY", true, "Enable shift right clicking book and quill on a block to receive inventory book").getBoolean();
+
+    config.save();
+  }
+}
---
+++ 1.9.4/src/main/java/ethanjones/mc/inventorybook/handler/ModInventoryHandlers.java
@@ -0,0 +1,53 @@
+package ethanjones.mc.inventorybook.handler;
+
+import baubles.api.BaublesApi;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.fml.common.registry.GameData;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ModInventoryHandlers {
+  public static class BaublesHandler extends PageHandler.IInventoryHandler {
+    @Override
+    public IInventory getIInventory(EntityPlayer entityPlayer) {
+      return BaublesApi.getBaubles(entityPlayer);
+    }
+
+    @Override
+    public ITextComponent title(EntityPlayer obj) {
+      return new TextComponentString("Baubles");
+    }
+  }
+
+  public static class BagginsesHandler extends PageHandler<ItemStack> {
+
+    @Override
+    public boolean valid(ItemStack obj) {
+      String n = obj.getItem().getRegistryName().toString().toLowerCase();
+      if (!n.startsWith("bagginses:") || n.equals("bagginses:void") || n.equals("bagginses:ender")) return false;
+      return obj.getTagCompound() != null && obj.getTagCompound().hasKey("Items");
+    }
+
+    @Override
+    public ItemStack itemStack(ItemStack obj, int i) {
+      NBTTagList items = obj.getTagCompound().getTagList("Items", 10);
+      return ItemStack.loadItemStackFromNBT(items.getCompoundTagAt(i));
+    }
+
+    @Override
+    public int itemStacksLength(ItemStack obj) {
+      NBTTagList items = obj.getTagCompound().getTagList("Items", 10);
+      return items.tagCount();
+    }
+
+    @Override
+    public ITextComponent title(ItemStack obj) {
+      return getItemStackComponent(obj, null, false, false);
+    }
+  }
+}
